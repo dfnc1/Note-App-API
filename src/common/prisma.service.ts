6d558d0cd4d2d920dc48aed682621fe69b6ad47e
dfnc1 <dianfnc06@gmail.com>
2026-06-16 14:55:11 +0700

    feat: create prisma databse service

---
+++ src/common/prisma.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaMariaDb({
+      database: process.env.DATABASE_URL,
+      connectionLimit: 5,
+    });
+    super({ adapter });
+  }
+}
