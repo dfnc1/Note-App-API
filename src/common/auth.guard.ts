7b9a1d28145110267efc1c0fa863dff574e73ca5
dfnc1 <dianfnc06@gmail.com>
2026-06-18 00:30:28 +0700

    feat: create guard for authentication

---
+++ src/common/auth.guard.ts
@@ -0,0 +1,27 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService, JsonWebTokenError, TokenExpiredError } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+  canActivate(context: ExecutionContext): boolean {
+    const request: Request = context.switchToHttp().getRequest();
+    const authHeader = request.headers['authorization'] as string;
+    const [type, token] = authHeader.split(' ');
+
+    if (type !== 'Bearer' || !token)
+      throw new UnauthorizedException('Unauthorized');
+
+    try {
+      request['user'] = this.jwtService.verifyAsync(token);
+    } catch {
+      throw new UnauthorizedException('Unauthorized');
+    }
+    return true;
+  }
+}
