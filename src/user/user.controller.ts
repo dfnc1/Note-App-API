5b702932faa1a654a97b74888e21c89a8d9d8588
dfnc1 <dianfnc06@gmail.com>
2026-06-17 18:06:37 +0700

    feat: create authentication controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import type { authResponse, userRequest } from './dto/user.dto';
+
+@Controller('/user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('/register')
+  @HttpCode(204)
+  async register(@Body() request: userRequest): Promise<authResponse> {
+    return await this.userService.register(request);
+  }
+
+  @Post('/login')
+  @HttpCode(200)
+  async login(@Body() request: userRequest): Promise<authResponse> {
+    return await this.userService.login(request);
+  }
+}
