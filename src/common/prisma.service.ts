28bd241139083afb5ae71e25dbd349de92b28b0e
dfnc1 <dianfnc06@gmail.com>
2026-06-17 17:27:18 +0700

    fix: resolve database error pool connection

--- src/common/prisma.service.ts
+++ src/common/prisma.service.ts
@@ -1,14 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import 'dotenv/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
+  private readonly logger: Logger = new Logger(PrismaService.name);
   constructor() {
     const adapter = new PrismaMariaDb({
-      database: process.env.DATABASE_URL,
-      connectionLimit: 5,
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      connectionLimit: Number(process.env.DATABASE_CONNECTION_LIMIT),
     });
-    super({ adapter });
+    super({
+      adapter,
+    });
   }
 }
