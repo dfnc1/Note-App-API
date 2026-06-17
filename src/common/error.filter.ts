291b1633206107283f65f0d12efbd9fdcc4e719b
dfnc1 <dianfnc06@gmail.com>
2026-06-18 00:28:40 +0700

    feat: create error filter

---
+++ src/common/error.filter.ts
@@ -0,0 +1,40 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { ZodError } from 'zod';
+import { Response } from 'express';
+import { JsonWebTokenError, TokenExpiredError } from '@nestjs/jwt';
+
+type FilterException = HttpException | ZodError | Error;
+
+@Catch(ZodError, HttpException, TokenExpiredError, JsonWebTokenError)
+export class ErrorFilter implements ExceptionFilter {
+  catch(exception: FilterException, host: ArgumentsHost) {
+    const response = host.switchToHttp().getResponse<Response>();
+
+    if (exception instanceof HttpException) {
+      response.status(exception.getStatus()).json({
+        errors: exception.getResponse(),
+      });
+    } else if (exception instanceof ZodError) {
+      response.status(400).json({
+        errors: 'Validation error',
+      });
+    } else if (exception instanceof TokenExpiredError) {
+      response.status(400).json({
+        errors: 'Token Expired Error',
+      });
+    } else if (exception instanceof JsonWebTokenError) {
+      response.status(400).json({
+        errors: 'Jwt Error',
+      });
+    } else {
+      response.status(501).json({
+        errors: exception.message,
+      });
+    }
+  }
+}
