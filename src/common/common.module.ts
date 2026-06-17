76fe0a969aac54bc8033d0a03ebc647c1b561680
dfnc1 <dianfnc06@gmail.com>
2026-06-18 00:32:32 +0700

    feat: sett JwtModule and add error filter

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { ValidationService } from './validation.service';
+import { APP_FILTER } from '@nestjs/core';
+import { ErrorFilter } from './error.filter';
+import { JwtModule } from '@nestjs/jwt';
 
+@Global()
 @Module({
-  providers: [PrismaService, ValidationService],
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '15m' },
+    }),
+  ],
+  providers: [
+    PrismaService,
+    ValidationService,
+    {
+      provide: APP_FILTER,
+      useClass: ErrorFilter,
+    },
+  ],
   exports: [PrismaService, ValidationService],
 })
 export class CommonModule {}
