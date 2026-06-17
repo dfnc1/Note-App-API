cb1fc0747d07b7d4abf89db026da92f405cb56dd
dfnc1 <dianfnc06@gmail.com>
2026-06-17 17:25:22 +0700

    refactor: change schema

--- prisma/migrations/20260616133521_create_note_tale/migration.sql
+++ prisma/migrations/20260616133521_create_note_tale/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
 CREATE TABLE `notes` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(100) NOT NULL,
-    `content` TEXT NOT NULL,
-    `userId` INTEGER NOT NULL,
+    `title` VARCHAR(100) NULL,
+    `content` TEXT NULL,
+    `user_id` INTEGER NOT NULL,
     `create_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `update_at` DATETIME(3) NOT NULL,
 
@@ -11,4 +11,4 @@ CREATE TABLE `notes` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `notes` ADD CONSTRAINT `notes_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `notes` ADD CONSTRAINT `notes_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
