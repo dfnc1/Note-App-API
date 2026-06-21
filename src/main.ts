2a739f3ca021a5e01bab7cb8b1abcc64c5ddadbb
dfnc1 <dianfnc06@gmail.com>
2026-06-21 10:17:43 +0700

    refactor: set app port

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
-import {ConsoleLogger} from "@nestjs/common";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConsoleLogger } from '@nestjs/common';
+import 'dotenv';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule,
-        {
-            logger: new ConsoleLogger({
-                json: true,
-            }),
-        });
-    await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      json: true,
+    }),
+  });
+  await app.listen(Number(process.env.PORT));
 }
 
 bootstrap().catch((err) => {
-    console.error(err);
+  console.error(err);
 });
